4f772826aa77c764bfa51464909987c884387b60
a.bordiug <a.bordiug@gmail.com>
2024-01-24 19:18:26 +0200

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,38 @@
 const {src, dest} = require('gulp');
 const fileinclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 
-function defaultTask() {
-
-}
+function defaultTask() {}
 
 function htmlTask() {
-    return src('src/*.html')
-        .pipe(fileinclude({prefix: '@@'}))
-        .pipe(dest('dist'));
+  return src('src/*.html')
+    .pipe(fileinclude({prefix: '@@'}))
+    .pipe(dest('dist'));
 }
 
 function stylesTask() {
-    return src('src/scss/*.scss')
-        .pipe(sass({ outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(dest('dist/css'));
+  return src('src/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('dist/css'));
+}
+
+function jsTask() {
+  return src('src/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('dist/js'));
 }
 
 exports.htmlTask = htmlTask;
-exports.default = defaultTask;
 exports.stylesTask = stylesTask;
+exports.jsTask = jsTask;
+
+exports.default = defaultTask;
